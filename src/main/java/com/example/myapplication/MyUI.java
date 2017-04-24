c0775ab0521b32b1300218d343345212bd5b74c1
channbora <channbora@gmail.com>
2017-04-24 17:35:20 +0700

    search

--- src/main/java/com/example/myapplication/MyUI.java
+++ src/main/java/com/example/myapplication/MyUI.java
@@ -1,17 +1,24 @@
 package com.example.myapplication;
 
+import java.util.List;
+
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -22,21 +29,59 @@ import com.vaadin.ui.VerticalLayout;
  */
 @Theme("mytheme")
 public class MyUI extends UI {
+	
+	private CustomerService service = CustomerService.getInstance();
+	private Grid grid = new Grid();
+	private TextField txtSearch = new TextField();
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        Label lblShow = new Label("Click me to alert value in the field : ");
+    	
+    	// basic component 
+       /* Label lblShow = new Label("Click me to alert value in the field : ");
         TextField txtShow = new TextField();
         Button btnShow = new Button("Show");
-        btnShow.addClickListener(e -> Notification.show(txtShow.getValue()));
+        btnShow.addClickListener(e -> Notification.show(txtShow.getValue()));*/
+    	
+    	Label lblSearch = new Label("Search");
+    	txtSearch.setInputPrompt("Name...");
+    	Button clearFilterBtn = new Button(FontAwesome.TIMES); //new Button("Clear");
+    	clearFilterBtn.addClickListener(e ->{
+    		txtSearch.clear();
+    		updateList();
+    	});
+    	
+    	txtSearch.addTextChangeListener(e -> {
+    		grid.setContainerDataSource(new BeanItemContainer<>(Customer.class, service.findAll(e.getText())));
+    	});
+    	
+    	grid.setColumns("id", "firstName","lastName", "email");
         VerticalLayout layout = new VerticalLayout();
+        //HorizontalLayout horizLayout = new HorizontalLayout();
+        //horizLayout.addComponents(txtSearch,clearFilterBtn);
+        
+    	CssLayout horizLayout = new CssLayout();
+    	horizLayout.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+    	horizLayout.addComponents(txtSearch,clearFilterBtn);
+    	
+        layout.addComponents(horizLayout, grid);
+        
+        updateList();
+        
+        
         layout.setMargin(true);
         layout.setSpacing(true);
-        layout.addComponents(lblShow, txtShow, btnShow);
+      //  layout.addComponents(lblShow, txtShow, btnShow);
         setContent(layout);
     }
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    private void updateList() {
+		// TODO Auto-generated method stub
+    	List<Customer> findAll = service.findAll();
+    	grid.setContainerDataSource(new BeanItemContainer<>(Customer.class, findAll));
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
